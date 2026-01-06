4273b94cb03a7b3d0876c2cd6e3370120c8c14c8
tapioNiemela80 <tapio_niemela1@yahoo.com>
2026-01-06 21:16:51 +0200

    dropped contact_persons table

--- src/main/java/tn/demo/project/view/ProjectViewRepository.java
+++ src/main/java/tn/demo/project/view/ProjectViewRepository.java
@@ -14,7 +14,7 @@ interface ProjectViewRepository extends Repository<ProjectTaskRow, UUID> {
                 p.initial_estimated_time_hours as project_estimate_hours,
                 p.initial_estimated_time_minutes as project_estimate_minutes,
                 p.status as project_status,
-                cp.email AS contact_person_email,
+                p.contact_person_email AS contact_person_email,
                 pt.id AS task_id,
                 pt.title AS task_title,
                 pt.description AS task_description,
@@ -24,7 +24,6 @@ interface ProjectViewRepository extends Repository<ProjectTaskRow, UUID> {
                 pt.actual_time_spent_hours as actual_hours,
                 pt.actual_time_spent_minutes as actual_minutes
                 FROM project_demo.projects p
-                LEFT JOIN project_demo.contact_persons cp ON p.id = cp.project_id
                 LEFT JOIN project_demo.project_tasks pt ON p.id = pt.project_id
                 WHERE p.id = :projectId
             """)
---
+++ src/main/resources/db/migration/V4__drop_unused_table.sql
@@ -0,0 +1,1 @@
+DROP TABLE project_demo.contact_persons;
